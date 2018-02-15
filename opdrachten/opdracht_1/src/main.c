2b659b372805f7524577fbb2024e80a3b80a89b6
Zowie van Geest <zowie@zowievangeest.nl>
2018-02-15 14:17:49 +0100

    Added define

--- opdrachten/opdracht_1/src/main.c
+++ opdrachten/opdracht_1/src/main.c
@@ -6,15 +6,24 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+// Define voor blink delay tijd in ms (1200)
+#define DELAY_BLINK_MS 1200
+
 int main(void)
 {
     // Pin D5 & D6 zijn de output pins voor de leds
     DDRD = 0b01100000;
 
+    // While loop
     while (1)
     {
+        // Zet led aan op D5 en D6 uit indien die aan was
         PORTD ^= 1 << 5;
-        _delay_ms(1200);
+
+        // Wacht 1.2 seconden
+        _delay_ms(DELAY_BLINK_MS);
+
+        // Zet led uit op D5 en zet led aan op D6
         PORTD ^= 1 << 6;
     }
 
