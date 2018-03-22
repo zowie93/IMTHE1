8def9577e94d96031bebcb31ebf99c37de1640ed
Zowie van Geest <zowie@zowievangeest.nl>
2018-03-22 15:20:36 +0100

    Added opdracht 4.3

---
+++ opdrachten/opdracht_4_3/src/main.c
@@ -0,0 +1,168 @@
+/**
+ * IMTHE1 - Zowie van Geest - 1097398 - INF3C
+ * Opdracht 4.3 - Led there be light
+ */
+
+// Baud rate.
+#define BAUD 9600
+
+// Includes.
+#include <avr/io.h>
+#include <util/setbaud.h>
+#include <avr/interrupt.h>
+
+// int voor teller.
+int teller = 0;
+
+// character begint op R rood.
+char kleurChar = 'r';
+
+// Gekopieerd uit bestand USART snap niet waarom dit moet als we het nu juist over code kwaliteit hebben en object georienteerd programmeren? 
+void initUSART(void)
+{
+    UBRR0H = UBRRH_VALUE;
+    UBRR0L = UBRRL_VALUE;
+
+    #if USE_2X
+        UCSR0A |= (1 << U2X0);
+    #else
+        UCSR0A &= ~(1 << U2X0);
+    #endif
+
+    // RXCIE0 zorgt ervoor dat de interrupt triggert op character.
+    UCSR0B = (1 << RXCIE0) | (1 << TXEN0) | (1 << RXEN0);
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+}
+
+// Gekopieerd uit bestand USART snap niet waarom dit moet als we het nu juist over code kwaliteit hebben en object georienteerd programmeren? 
+uint8_t receiveByte(void)
+{
+    loop_until_bit_is_set(UCSR0A, RXC0);
+    return UDR0;
+}
+
+// Gekopieerd uit bestand USART snap niet waarom dit moet als we het nu juist over code kwaliteit hebben en object georienteerd programmeren? 
+void transmitByte(uint8_t data)
+{
+    loop_until_bit_is_set(UCSR0A, UDRE0);
+    UDR0 = data;
+}
+
+// Gekopieerd uit bestand USART snap niet waarom dit moet als we het nu juist over code kwaliteit hebben en object georienteerd programmeren? 
+void printString(const char myString[])
+{
+    uint8_t i = 0;
+    while (myString[i])
+    {
+        transmitByte(myString[i]);
+        i++;
+    }
+}
+
+void initTimerOverflow()
+{
+    // Mask van de timer
+    TIMSK0 |= (1 << TOIE0);
+
+    // Instellingen timer
+    TCCR0B |= (1 << CS02) | (1 << CS00);
+}
+
+void initPWMTimerLED()
+{
+    // Timer 1 R-Led en G-Led
+    TCCR1A |= (1 << WGM10) | (1 << WGM12); // Fast PWM.
+    TCCR1B |= (1 << CS11);                 // PWM freq.
+
+    // Timer 2 B-Led
+    TCCR2A |= (1 << WGM20) | (1 << WGM21); // Fast PWM.
+    TCCR2B |= (1 << CS21);                 // PWM freq.
+
+    // R-Led en G-Led
+    TCCR1A |= (1 << COM1A1) | (1 << COM1B1);
+
+    // B-Led
+    TCCR2A |= (1 << COM2A1);
+}
+
+// Als er geschreven wordt naar de seriele verbinding.
+ISR (USART_RX_vect)
+{
+    if(UDR0 == 'r') {
+        // Print de kleur
+        printString("kleur veranderd naar rood\n");
+        // Teller op 0 zetten
+        teller = 0;
+        // Opslaan in de kleurChar
+        kleurChar = 'r';
+    } else if(UDR0 == 'g') {
+        // Print de kleur
+        printString("kleur veranderd naar groen\n");
+        // Teller op 0 zetten
+        teller = 0;
+        // Opslaan in de kleurChar
+        kleurChar = 'g';
+    } else if(UDR0 == 'b') {
+        // Print de kleur
+        printString("kleur veranderd naar blauw\n");
+        // Teller op 0 zetten
+        teller = 0;
+        // Opslaan in de kleurChar
+        kleurChar = 'b';
+    } else {
+        // Print wat voor input er gegeven moet worden.
+        printString("Vul een R in voor Rood een G voor Groen en een B voor Blauw\n");
+    }
+}
+
+// Timer overflow interrupt.
+ISR (TIMER0_OVF_vect)
+{
+    // Tel de count op, voor de brightness.
+    teller++;
+    // Switch om te kijken om welke kleur het gaat.
+
+    if(kleurChar == 'r') {
+      // Rode led aan met teller
+      OCR1A = ~teller;
+      // Groene led uit
+      OCR1B = ~0;
+      // Blauwe led uit
+      OCR2A = ~0;
+    } else if(kleurChar == 'g') {
+      // Rode led aan met teller
+      OCR1A = ~0;
+      // Groene led uit
+      OCR1B = ~teller;
+      // Blauwe led uit
+      OCR2A = ~0;
+    } else if(kleurChar == 'b') {
+      // Rode led aan met teller
+      OCR1A = ~0;
+      // Groene led uit
+      OCR1B = ~0;
+      // Blauwe led uit
+      OCR2A = ~teller;
+    }
+}
+
+
+// Main methode.
+int main(void)
+{
+    // RGB pinnen initialiseren
+    DDRB = (1 << PB1) | (1 << PB2) | (1 << PB3);
+    // USART initialiseren
+    initUSART();
+    // Functie van de overflow timer
+    initTimerOverflow();
+    // Functie van de led timer
+    initPWMTimerLED();
+    // Interrupts aan
+    sei();
+
+    while (1) {
+    }
+
+    return 0;
+}
