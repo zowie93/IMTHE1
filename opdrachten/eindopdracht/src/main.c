51797815b6953618c68c623b2e7a2864210afda6
Zowie van Geest <zowie@zowievangeest.nl>
2018-04-02 11:56:53 +0200

    Added comments

--- opdrachten/eindopdracht/src/main.c
+++ opdrachten/eindopdracht/src/main.c
@@ -11,25 +11,38 @@
 #include <avr/interrupt.h>
 #include <USART.h>
 
+// DHT11 pinnen
 #define DHT11_PIN 6
+// Pinnen waar data overheen gaat shift register
 #define DS_PORT PORTC
 #define DS_PIN 0
+// ST CP port en pin van shiftregister
 #define ST_CP_PORT PORTC
 #define ST_CP_PIN 1
+// SH CP port en pin van shiftregister
 #define SH_CP_PORT PORTC
 #define SH_CP_PIN 2
 
+// Delay van de ledjes
 #define DELAY_LED 160
 #define DELAY_LED_K 110
 
+// Pin laag zetten
 #define DS_low() DS_PORT &= ~_BV(DS_PIN)
+// Pin hoog zetten
 #define DS_high() DS_PORT |= _BV(DS_PIN)
+// ST CP pin laag
 #define ST_CP_low() ST_CP_PORT &= ~_BV(ST_CP_PIN)
+// ST CP pin hoog
 #define ST_CP_high() ST_CP_PORT |= _BV(ST_CP_PIN)
+//SH CP pin laag
 #define SH_CP_low() SH_CP_PORT &= ~_BV(SH_CP_PIN)
+// SH CP pin hoog
 #define SH_CP_high() SH_CP_PORT |= _BV(SH_CP_PIN)
 
+// variable voor Integraal Luchtvochtigheid, Decimaal Luchtvochtigheid, Integraal Temperatuur, Decimaal temperatuur en als laatste de checksum.
 uint8_t c = 0, I_RH, D_RH, I_Temp, D_Temp, CheckSum;
+// Variable voor het optellen
 int count;
 
 // Functie voor het starten van pull-up-pull-down request
@@ -129,44 +142,59 @@ void matrixInit()
     PORTC = 0b00000000;
 }
 
+// Functie waarbij je 1 ledje aanzet met berekening van 1 rij en 1 column
 void single_led(int row, int column)
 {
+    // Zet SH CP op low
     SH_CP_low();
+    // Zet ST CP op low
     ST_CP_low();
+    // For loop die het matrix indexeerd van 8 rijen
     for (int i = 0; i < 8; i++)
     {
+        // Als i een getal is gaat de pin op high dus rij x column is ledje aan
         if (i == row)
         {
+            // Zet pin hoog
             DS_high();
         }
-
+        // Anders nog iets?
         else
         {
+            // Zet pin laag
             DS_low();
         }
-
+        // Pin hoog
         SH_CP_high();
+        // Pin laag
         SH_CP_low();
     }
 
+    // For loop die het matrix indexeerd van 8 columns
     for (int i = 0; i < 8; i++)
     {
+        // Als i een getal is gaat de pin op high dus rij x column is ledje aan
         if (i == column)
         {
+            // Zet pin laag
             DS_low();
         }
 
         else
-        {
+        {   
+            // Zet ping hoog
             DS_high();
         }
-
+        // Pin hoog
         SH_CP_high();
+        // Pin laag
         SH_CP_low();
     }
+    // Pin hoog eindigen 
     ST_CP_high();
 }
 
+// Functie voor het tekenen van een K
 void drawKoorts()
 {
     single_led(0, 0);
@@ -235,6 +263,7 @@ void drawKoorts()
     _delay_us(DELAY_LED_K);
 }
 
+// Functie voor het tekenen van het kruis
 void drawGeenKoorts()
 {
     _delay_us(DELAY_LED);
@@ -284,56 +313,83 @@ void drawGeenKoorts()
     _delay_us(DELAY_LED);
 }
 
+// Timer 1
 ISR(TIMER0_OVF_vect)
 {
+    // Kijken wanneer de count groter is dan 300 dan voert die dit uit
     if (count > 300)
     {
+        // Voor telkens opnieuw uitvoeren zet ik die count op 0 als die in de loop zit
         count = 0;
 
+        // Data charset voor alle waardes uit de DHT11
         char data[5];
 
+        // Starten van de meting
         dht11_measure();
 
+        // Als de waardes niet overeen komen met de bijbehorende checksum print die een error.
         if ((I_RH + D_RH + I_Temp + D_Temp) != CheckSum)
         {
+            // Print error.
             printString("Error");
         }
-
+        // Anders nog iets?
         else
         {
+            // Omzetten van de data
             itoa(I_RH, data, 10);
+            // Printen data
             printString(data);
+            // punt als tussenwaarde
             printString(".");
 
+            // Omzetten van de data
             itoa(D_RH, data, 10);
+            // Printen data
             printString(data);
+            // Luchtvochtigheid is in percentage
             printString("%");
 
+            // Omzetten van de data
             itoa(I_Temp, data, 10);
+            // Printen data
             printString(data);
+            // punt als tussen waarde
             printString(".");
 
+            // Omzetten van de data
             itoa(D_Temp, data, 10);
+            // Printen data
             printString(data);
+            // Celsuis
             printString("C ");
 
+            // Omzetten van de data
             itoa(CheckSum, data, 10);
+            // Printen data
             printString(data);
+            // New line
             printString("\n\n");
         }
     } else {
+        // Count optellen
         count++;
     }
 }
 
+// Tweede timer die checkt of de waarde gelijk is als statement
 ISR(TIMER1_OVF_vect)
 {
+    // Als de temperatuur hoger is van 37 graden celsius
     if (I_Temp > 37)
-    {
+    {   
+        // Teken koorts symbool
         drawKoorts();
     }
     else
     {
+        // Teken smiley symbool
         drawGeenKoorts();
     }
 }
@@ -341,12 +397,16 @@ ISR(TIMER1_OVF_vect)
 int main(void)
 {
 
+    // USART initialiseren
     initUSART();
 
+    // 1 Seconden delay
     _delay_ms(1000);
 
+    // Matrix initialiseren
     matrixInit();
 
+    // Timer overflow initialiseren
     initTimerOverflowCapture();
 
     while (1)
